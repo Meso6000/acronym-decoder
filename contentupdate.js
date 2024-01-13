0e7b750be41a538d32d9368b1f5a0ec70003df8c
RedSovietCapitalist <alinaseri2907@gmail.com>
2024-01-13 02:06:49 -0800

    outline of parsing method

--- contentupdate.js
+++ contentupdate.js
@@ -10,20 +10,47 @@ let localAcrynoms = {
 // sanity check the words and acrynom
 // save acrynom + words into dictionary
 
-//scan page for acronyms looking for
-
 for (let i = 0; i < parts.length; i++){
     var part = parts[i];
+
     for (let j = 0; j < part.childNodes.length; j++){
         let node = part.childNodes[j];
+
         if (node.nodeType === 3){
             var text = node.nodeValue;
-            var replacedText = text.replace(/Google/gi, 'Gbuddy');
+            var words = text.split(" ");
+            let reading = false;
+            let number = 0;
+            let acrynomReading = 0
 
+            for (let k = 0; k < words.length; k++){
+                if (words[k].includes("(")){
+                    reading = true;
+                    acrynomReading = k;
+                    number++;
+                }
+                else if(words[k].includes(")")){
+                    reading = false;
+                    acronymDef = ""
+
+                    for (let l = k - number; l <= k; l++){
+                        acronymDef += words[l];
+                    }
+                    
+                    localAcrynoms[words[acrynomReading]] = acronymDef;
+                    acrynomReading = "";
+                    number = 0;
+                }
+                else if (reading){
+                    number++;
+                }
+            };
+            
+            var replacedText = text.replace(/Google/gi, 'Gbuddy');
+            replacedText += number;
             if (replacedText !== text) {
                 part.replaceChild(document.createTextNode(replacedText), node);
             }
-            var words = text.split(" ");
         }
     }
 }
